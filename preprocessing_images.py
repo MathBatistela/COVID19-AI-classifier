f722f79a76270d810b28a7561a3cd6529c24a1c8
Matheus Batistela <matheushenriquebatistela@gmail.com>
2020-11-18 17:17:21 -0300

    deep featuresextractor

--- preprocessing_images.py
+++ preprocessing_images.py
@@ -9,21 +9,28 @@ from skimage import exposure
 
 def main(source_dir):
     folder_name = os.path.basename(os.path.normpath(source_dir))
-    destination_dir = os.getcwd() + "\\dataset\\preprocessed_images\\" + folder_name
+    destination_dir = "dataset/processed_images/"
+    
+    if folder_name == "normal":
+        destination_dir = destination_dir + "f1"
+    elif folder_name == "covid":
+        destination_dir = destination_dir + "f2"
+        
     if not os.path.exists(destination_dir):
         os.mkdir(destination_dir)
     
-    image_list=os.listdir(source_dir)
+    img_list=os.listdir(source_dir)
     
-    for img_name in image_list:
+    for img_name in img_list:
         img=io.imread(os.path.join(source_dir,img_name))
         img_gray = rgb2gray(img)
-        img_resized = resize(img_gray, (600,590))#convert image size to 512*512
-        img_rescaled=(img_resized-np.min(img_resized))/(np.max(img_resized)-np.min(img_resized))#min-max normalization 
-        img_enhanced=exposure.equalize_adapthist(img_rescaled)#adapt hist
+        img_resized = resize(img_gray, (590,600))
+        img_rescaled=(img_resized-np.min(img_resized))/(np.max(img_resized)-np.min(img_resized))
+        img_enhanced=exposure.equalize_adapthist(img_rescaled)
         img_resized_8bit=img_as_ubyte(img_enhanced)
-        # img_name = "exposure" + img_name
-        io.imsave(os.path.join(destination_dir,img_name),img_resized_8bit)#save enhanced image to destination dir
+        img_name = img_name[:-3] + "bmp"
+        print(img_name)
+        io.imsave(os.path.join(destination_dir,img_name),img_resized_8bit)
         
 
 if __name__ == "__main__":
